ffe1b34d65098aa08b2a33ec0cd38cc94cd86f57
Ben Hanna <benpaulhanna@gmail.com>
2019-09-13 11:19:58 -0400

    Updated tests.

--- test/web_test/core_test.clj
+++ test/web_test/core_test.clj
@@ -2,14 +2,17 @@
   (:require [clojure.test :refer :all]
             [web-test.core :refer :all]))
 
+(defn status-ok? [response] (= 200 (:status response)))
+(defn status-not-found? [response] (= 404 (:status response)))
+
 (deftest route-home
   (testing "Match the home route."
     (let [response (handler {:request-method :get :uri "/"})]
-      (is (= 200 (:status response)))
+      (is (status-ok? response))
       (is (= "Home" (:body response))))))
 
 (deftest route-not-found
   (testing "Fallback when no route is found."
     (let [response (handler {:request-method :get :uri "/random"})]
-      (is (= 404 (:status response)))
+      (is (status-not-found? response))
       (is (= "Not Found" (:body response))))))
