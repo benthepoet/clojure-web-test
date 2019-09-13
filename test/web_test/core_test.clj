dbe15adf5fe6ff1b6eb39352c6d39d70e5f451d2
Ben Hanna <benpaulhanna@gmail.com>
2019-09-13 10:31:00 -0400

    Added tests for coverage.

--- test/web_test/core_test.clj
+++ test/web_test/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [web-test.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest route-home
+  (testing "Match the home route."
+    (let [response (handler {:request-method :get :uri "/"})]
+      (is (= 200 (:status response)))
+      (is (= "Home" (:body response))))))
+
+(deftest route-not-found
+  (testing "Fallback when no route is found."
+    (let [response (handler {:request-method :get :uri "/random"})]
+      (is (= 404 (:status response)))
+      (is (= "Not Found" (:body response))))))
